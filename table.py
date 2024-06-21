6a4ca2ab3b3e1301e756553b6363ab280a87f91d
sudo-mrahn <alexander.ahn@pennington.org>
2024-06-21 15:02:43 -0400

    testing NiceGUI

--- table.py
+++ table.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 from nicegui import events, ui
+import matplotlib.pyplot as plt
+import curve
+import pandas as pd
 
 columns = [
     {'name': 'name', 'label': 'Name', 'field': 'name', 'align': 'left'},
@@ -72,5 +75,32 @@ with table.add_slot('bottom-row'):
         ui.button('Add row', icon='add', color='accent', on_click=add_row).classes('w-full')
 table.on('rename', rename)
 table.on('delete', delete)
+
+with ui.pyplot():
+    
+
+# with ui.pyplot():
+
+#     fig, axs = plt.subplots(nrows=4, ncols=2)
+#     fig.set_size_inches(12,10)
+#     arr = [orig, floor, max, med]
+#     grades = [
+#         pd.DataFrame.from_dict(curve.grades(orig), orient='index'), 
+#         pd.DataFrame.from_dict(curve.grades(floor), orient='index'), 
+#         pd.DataFrame.from_dict(curve.grades(max), orient='index'), 
+#         pd.DataFrame.from_dict(curve.grades(med), orient='index')]
+#     bins=80
+#     names = ['raw scores', 'simple floor', 'scale to max', 'scale to median']
+#     for i in range(4):
+#         axs[i,0].hist(arr[i], bins, density=True, label='avg: ' + str(round(np.average(arr[i]))))
+#         axs[i,0].axvline(x=np.median(arr[i]), ymin=0, ymax=0.85, color='red', label='median: ' + str(round(np.median(arr[i]))))
+#         axs[i,0].set_xlim([min(arr[0]), 100])
+#         axs[i,0].set_ylabel(names[i])
+#         axs[i,0].yaxis.label.set_fontsize(14)
+#         axs[i,0].legend()
+#         grades[i] = grades[i][::-1]
+#         grades[i].plot(kind='bar', ax=axs[i,1])
+#         axs[i,1].legend(['min: ' + str(round(min(arr[i])))])
+
 
 ui.run()
